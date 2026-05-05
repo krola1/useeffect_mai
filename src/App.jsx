0d84122ea7c848a0b272936747131df402c63986
krola1 <kwl082024@gmail.com>
2026-05-05 10:42:32 +0200

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import Timer from "./components/Timer";
 import User from "./components/User";
+import { useEffect } from "react";
 export default function App() {
   const [showTimer, setShowTimer] = useState(false);
+
+  //useeffect tar inn to argumenter,
+  //første er en anonym funksjon med en kodeblokk
+  //den andre er et dependency array som definerer når koden skal kjøres, tom array = on first mount
+  useEffect(() => {
+    // alt i return kodeblokk kjøres på unmount
+    return () => {};
+  }, []);
 
   return (
     <>
