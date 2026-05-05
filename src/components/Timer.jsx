b9611d66b8710fb08463c970d43aea0387f55111
krola1 <kwl082024@gmail.com>
2026-05-05 10:21:04 +0200

    bang

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,17 +4,20 @@ import { useState } from "react";
 export default function Timer() {
   const [seconds, setSeconds] = useState(0);
 
+  //useffect with empty dependency array, runs only on mount
   useEffect(() => {
+    //sets counde to be run at interval
     const intervalID = setInterval(() => {
       setSeconds((prev) => prev + 1);
       console.log("tick");
     }, 1000);
 
+    //return is triggered on unmount
     return () => {
       clearInterval(intervalID);
       console.log("cleanup");
     };
-  }, []);
+  }, []); /// dependency array defines when code is run, empty aray == initial mount)
   return (
     <>
       <h3>Timer:{seconds}</h3>
